249c4817fd3fca8918c5973c3175116607640910
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-04-07 02:57:13 +0600

    button adde and btn handler

--- src/component/TShirt/TShirt.jsx
+++ src/component/TShirt/TShirt.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import "./TShirt.css";
 
-const TShirt = ({ tshirt }) => {
+const TShirt = ({ tshirt, handleAddToCart }) => {
   const { picture, name, price } = tshirt;
   return (
-    <div>
+    <div className="t-shirt">
       <img src={picture} alt="" />
+      <h4>{name}</h4>
+      <p>Price: ${price}</p>
+      <button>Buy Now</button>
     </div>
   );
 };
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 import TShirt from "../TShirt/TShirt";
+import Cart from "../Cart/Cart";
+import "./Home.css";
 
 const Home = () => {
   const tshirts = useLoaderData();
+  const [cart, setCart] = useState([]);
+
+  const handleAddToCart = (tshirt) => {};
+
   return (
-    <div>
-      <h2>this is home: {tshirts.length}</h2>
-      {tshirts.map((tshirt) => (
-        <TShirt key={tshirt._id} tshirt={tshirt}></TShirt>
-      ))}
+    <div className="home-container">
+      <div className="t-shirt-container">
+        {tshirts.map((tshirt) => (
+          <TShirt
+            key={tshirt._id}
+            tshirt={tshirt}
+            handleAddToCart={handleAddToCart}
+          ></TShirt>
+        ))}
+      </div>
+      <div className="cart-container">
+        <Cart></Cart>
+      </div>
     </div>
   );
 };
